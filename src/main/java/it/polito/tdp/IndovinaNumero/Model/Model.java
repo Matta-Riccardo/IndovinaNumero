50906a9866d7ee623484b80d6638d6fc5fdaf7c4
riccardo <riccardo@172.22.103.146>
2022-03-16 14:24:40 +0100

    new

---
+++ src/main/java/it/polito/tdp/IndovinaNumero/Model/Model.java
@@ -0,0 +1,75 @@
+package it.polito.tdp.IndovinaNumero.Model;
+
+import java.security.InvalidParameterException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Model {
+	
+	//Variabili del modello
+	
+	private int segreto;
+	private final int TMAX = 8;
+	private final int NMAX = 100;
+	private int tentativiFatti;
+	private Set<Integer> tentativi;
+	private boolean inGioco = false;
+	
+	
+	public void nuovaPartita() {
+		tentativi = new HashSet<Integer>();
+		//gestione di una nuova partita
+    	this.segreto = (int)((Math.random() * NMAX) +1);
+    	this.tentativiFatti = 0;	
+    	this.inGioco = true;
+	}
+	
+	public int tentativo(int tentativo) {
+		
+		if(!inGioco)
+			throw new IllegalStateException("HAI PERSO! La partita è terminata");
+		
+		if(!tentativoValido(tentativo))
+			throw new InvalidParameterException("Devi inserire un numero tra 1 e " +NMAX+ " che non hai mai utilizzato");
+		
+		this.tentativi.add(tentativo);
+		this.tentativiFatti++;
+		
+		if(this.tentativiFatti == TMAX) {
+			this.inGioco = false;
+		}
+		
+		if(tentativo == segreto) {
+			this.inGioco = false;
+			return 0;
+		}else if(tentativo < segreto) {
+			return -1;
+		}else {
+			return 1;
+		}
+		
+	}
+
+	private boolean tentativoValido(int tentativo) {
+		if(tentativo < 1 || tentativo > NMAX || tentativi.contains(tentativo))
+			return true;
+		return false;
+	}
+
+	public int getSegreto() {
+		return segreto;
+	}
+
+	public int getTMAX() {
+		return TMAX;
+	}
+
+	public int getNMAX() {
+		return NMAX;
+	}
+
+	public int getTentativiFatti() {
+		return tentativiFatti;
+	}
+	
+}
---
+++ src/main/java/it/polito/tdp/IndovinaNumero/Model/tentativi.java
@@ -0,0 +1,5 @@
+package it.polito.tdp.IndovinaNumero.Model;
+
+public class tentativi {
+
+}
